17e295b64a57b5103118c357d0d5ec6e181b1635
yifen_000 <yifen_000@192.168.39.212>
2016-11-26 15:50:31 +1100

    RunServer.java modification

--- src/main/RunServer.java
+++ src/main/RunServer.java
@@ -1,49 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package main;
 import java.security.GeneralSecurityException;
 import org.kohsuke.args4j.Option;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.CmdLineException;
-/**
- *
- * @author s3931660
- */
+
 public class RunServer {
     
+    //parse the command line arguments
     @Option(name="-p",usage="server port number")
     public String port="";
     
-        public static void main(String[] args) throws GeneralSecurityException{
-            RunServer rs = new RunServer();
-            CmdLineParser parser = new CmdLineParser(rs);
+    public static void main(String[] args) throws GeneralSecurityException, CmdLineException {
+        new RunServer().doMain(args);
+    }
+    
+    public void doMain(String[] args) throws CmdLineException, GeneralSecurityException{
+        CmdLineParser parser = new CmdLineParser(this);
+        parser.parseArgument(args);
+        this.run(port);
+    }
+    
+    
+    // initiate the server program
+    public void run(String port) throws GeneralSecurityException {
 
-       try {
-                    parser.parseArgument(args);
-                    rs.run();
-            } catch (CmdLineException e) {
-                // handling of wrong arguments
-                System.err.println(e.getMessage());
-                parser.printUsage(System.err);
-            }
-        }
-        
-    public void run() throws GeneralSecurityException{
-        
-       int portNumber = 4444;
-       
-       if(!port.isEmpty()){
+        int portNumber = 4444;
+
+        if (port!="") {
             portNumber = Integer.parseInt(port);
         }
 
         Server chatRooms = new Server(portNumber);
-        
+
         chatRooms.creatRoom("room1");
         chatRooms.creatRoom("comp90015");
 
         chatRooms.start();
     }
 }
+
+
